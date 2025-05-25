73ee5cae1bb37d24b2ce1a5bd3bb1f55c9ad467b
Karancoding11 <119086220+Karancoding11@users.noreply.github.com>
2025-05-25 23:06:11 +0530

    login page

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
+
+
 let currentSong = new Audio;
 let songsList = [];
 let currentIndex = 0;
 
 async function getsongs() {
-    let a = await fetch("http://127.0.0.1:5500/songs/");
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    let songs = [];
-
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split("/songs/")[1]);
-        }
-    }
-
-    return songs;
+    let res = await fetch("songs.json");
+    return await res.json();
 }
 
 const playMusic = (track, index) => {
-    currentSong.src = "/songs/" + track;
+    currentSong.src = "songs/" + track.title;
     currentSong.play();
     currentIndex = index;
     pause.src = "svg/media-pause.svg";
@@ -37,8 +26,8 @@ async function main() {
         <li data-index="${index}">
             <img src="svg/music-note-square-02-stroke-rounded.svg" alt="">
             <div class="info">
-                <div class="songName">${song.replaceAll("%20", " ")}</div>
-                <div class="songArtist">song artist</div>
+                <div class="songName">${song.title.replaceAll("%20", " ")}</div>
+                <div class="songArtist">${song.artist}</div>
             </div>
             <div class="playnow"> 
                 <span>Play Now</span><img id="playSong" src="svg/play.svg" alt="">
@@ -46,13 +35,13 @@ async function main() {
         </li>`;
     }
 
-    // Play selected song
     document.querySelectorAll(".songlist li").forEach((el) => {
         el.addEventListener("click", () => {
             let index = parseInt(el.getAttribute("data-index"));
             playMusic(songsList[index], index);
         });
     });
+
 
     // Play/Pause toggle
     pause.addEventListener("click", () => {
@@ -162,3 +151,4 @@ function onStopDrag(e) {
 
     document.removeEventListener("mousemove", onDrag);
 }
+// Add this to script.js
